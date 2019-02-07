e9d9d644be568164679ca42faf90d1e7dcbe7176
Bikash <gbikash34@gmail.com>
2019-02-07 12:24:54 +0545

    Login process modified

--- login.php
+++ login.php
@@ -1,6 +1,13 @@
 <?php
-$PAGE_TITLE = 'Login';
+require_once('app_init.php');
+
+if (Session::has('user')) {
+	redirect(url('admin'));
+}
+
+$PAGE_TITLE = "Login";
 require_once('header.php');
+$form = new Form('login', 'POST', url('login_process'));
 ?>
 
 <section>
@@ -9,17 +16,33 @@ require_once('header.php');
             <div class="col-md-6 col-md-offset-3">
                 <h3><?= $PAGE_TITLE ?></h3>
                 <hr>
-                <form>
+                <form action="<?= $form->action() ?>" method="<?= $form->method() ?>">
+                    <?= $form->get_meta_fields() ?>
+
                     <div class="form-group">
                         <label for="username">Username</label>
-                        <input type="text" class="form-control" id="username" name="username">
+                        <input type="text" class="form-control" id="username" name="username" value="<?= $form->value('username') ?>">
+                        <?php if ($form->has_errors('username')): ?>
+                            <?php foreach($form->errors('username') as $error): ?>
+                            <small class="text-danger"><?= $error ?></small><br>
+                            <?php endforeach ?>
+                        <?php endif; ?>
                     </div>
+
                     <div class="form-group">
                         <label for="password">Password</label>
                         <input type="password" class="form-control" id="password" name="password">
+                        <?php if ($form->has_errors('password')): ?>
+                            <?php foreach($form->errors('password') as $error): ?>
+                            <small class="text-danger"><?= $error ?></small><br>
+                            <?php endforeach ?>
+                        <?php endif; ?>
                     </div>
+
                     <br>
-                    <button type="submit" class="btn btn-default btn-primary btn-block"><?= $PAGE_TITLE ?></button>
+                    <button type="submit" class="btn btn-primary btn-block">Login</button>
+                    <br>
+                    <p><a href="<?= url('register') ?>">Create a new account</a></p>
                 </form>
             </div>
         </div>
---
+++ login_process.php
@@ -0,0 +1,23 @@
+<?php
+require_once('app_init.php');
+
+$username = strtolower(trim(Request::POST('username')));
+$password = Request::POST('password');
+
+$validator = new Validator('POST', url('login'));
+$validator->add_value('username', $username);
+
+if (empty($username)) {
+    $validator->add_error('username', 'This is a required field.');
+} else {
+    $user = User::getByUsername($username);
+    if (strtolower($user->username) !== strtolower($username)) {
+        $validator->add_error('username', 'That username does not exist.');
+    } else if (!password_verify($password, $user->password)) {
+        $validator->add_error('password', 'Wrong password.');
+    }
+}
+
+$validator->validate();
+
+Session::set('user', $user);
