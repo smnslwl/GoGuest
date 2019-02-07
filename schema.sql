261856c435a1f2fa69b8daa69b728a4468974d4c
Samin Silwal <smnslwl@gmail.com>
2019-02-07 12:47:57 +0545

    Added booking and location class and updated database schema

--- schema.sql
+++ schema.sql
@@ -5,3 +5,24 @@ CREATE TABLE  users(
     username VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS locations;
+
+CREATE TABLE locations(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    user INT NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    latitude DECIMAL(9, 6) NOT NULL,
+    longitude DECIMAL(9, 6) NOT NULL,
+    FOREIGN KEY (user) REFERENCES users (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS bookings;
+
+CREATE TABLE bookings(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    location INT NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    status INT DEFAULT 0,
+    FOREIGN KEY (location) REFERENCES locations (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
