fc849d3f2b45e91a5d87e3716f67b34683e80eb4
Bikash <gbikash34@gmail.com>
2019-02-07 11:57:54 +0545

    Modified Login form

--- login.php
+++ login.php
@@ -9,6 +9,18 @@ require_once('header.php');
             <div class="col-md-6 col-md-offset-3">
                 <h3><?= $PAGE_TITLE ?></h3>
                 <hr>
+                <form>
+                    <div class="form-group">
+                        <label for="username">Username</label>
+                        <input type="text" class="form-control" id="username" name="username">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" class="form-control" id="password" name="password">
+                    </div>
+                    <br>
+                    <button type="submit" class="btn btn-default btn-primary btn-block"><?= $PAGE_TITLE ?></button>
+                </form>
             </div>
         </div>
     </div>
