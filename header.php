4c61041dac520a34f81bc1bad02ba6988f8881c5
Samin Silwal <smnslwl@gmail.com>
2019-02-07 11:50:22 +0545

    Modified page title

--- header.php
+++ header.php
@@ -8,7 +8,7 @@ require_once('app_init.php');
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?= $APP_TITLE ?></title>
+    <title><?= $PAGE_TITLE . ' - ' . $APP_TITLE ?></title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
     <script src="js/jquery.min.js"></script>
