cb784a198c2c1dd98f48f95ab74839fdfa1c87bd
Samin Silwal <smnslwl@gmail.com>
2019-02-07 11:59:51 +0545

    Modified Session class

--- classes/Session.php
+++ classes/Session.php
@@ -3,6 +3,23 @@
 // Wrapper around the $_SESSION superglobal
 class Session {
 
+    public static function start()
+	{
+        if (session_status() == PHP_SESSION_NONE) {
+            session_start();
+        }
+
+        if (!isset($_SESSION['csrf_token']) || empty($_SESSION['csrf_token'])) {
+            $_SESSION['csrf_token'] = base64_encode(openssl_random_pseudo_bytes(32));
+        }
+	}
+
+    public static function end()
+	{
+        session_destroy();
+        unset($_SESSION);
+    }
+
     public static function has($key)
     {
         return array_key_exists($key, $_SESSION) ? true : false;
@@ -16,6 +33,16 @@ class Session {
     public static function set($key, $value)
     {
         $_SESSION[$key] = $value;
+    }
+
+    public static function get_csrf_token()
+    {
+        return $_SESSION['csrf_token'];
+    }
+
+    public static function verify_csrf_token($token)
+    {
+        return $_SESSION['csrf_token'] === $token;
     }
 
 }
